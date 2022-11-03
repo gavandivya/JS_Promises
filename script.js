5518aed4b249ea13ebbac7b6b6533e59e1ab2306
Divya R. Gavandi <divya190399@gmail.com>
2022-11-03 13:54:46 +0530

    initial

---
+++ script.js
@@ -0,0 +1,9 @@
+// Practising Promises
+// Promise is an object representing eventual completion or failure of an asyncronous operation
+
+//Task to be performed
+
+//Create Order
+//Proceed To Payment
+//Show Order Summary
+// Update wallet
